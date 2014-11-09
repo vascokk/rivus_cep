9b57a7dc6b75aec2cb672c252bd59460c01d7098
Vasco <vas@vas.io>
2014-11-09 13:38:29 +0000

    Updated basho bench driver and config

--- priv/basho_bench_driver_rivus.erl
+++ priv/basho_bench_driver_rivus.erl
@@ -8,35 +8,38 @@
 -record(state, {
     host,
     port,
-    socket
+    socket,
+    client_num
 }).
 
-new(1) ->
+new(Id) ->
     {Host, Port, Socket} = connect(),
-    deploy_queries(Socket),
+    deploy_queries(Socket, Id),
 
     {ok, #state{
         host   = Host,
         port   = Port,
-        socket = Socket}};
-new(_Id) ->
-    {Host, Port, Socket} = connect(),
-
-    {ok, #state{
-        host   = Host,
-        port   = Port,
-        socket = Socket}}.
+        socket = Socket,
+        client_num = Id}}.
+%% new(_Id) ->
+%%     {Host, Port, Socket} = connect(),
+%%
+%%     {ok, #state{
+%%         host   = Host,
+%%         port   = Port,
+%%         socket = Socket}}.
 
 connect() ->
     {Host, Port} = basho_bench_config:get(rivus_cep_tcp_serv, {"127.0.0.1", 5775}),
     {ok, Socket} = gen_tcp:connect(Host, Port, [{active, false}, {nodelay, true}, {packet, 4}, binary]),
     {Host, Port, Socket}.
 
-deploy_queries(Socket) ->
+deploy_queries(Socket, Id) ->
     Queries = basho_bench_config:get(rivus_cep_queries, []),
     lists:foreach(fun({Type, Query}) ->
-        lager:info("Deploying query type: ~p, stmt: ~p", [Type, Query]),
-        execute(Type, Query, Socket) end, Queries).
+        Q = re:replace(Query,"\\$",integer_to_list(Id),[global, {return,list}]),
+        lager:info("Deploying query type: ~p, client: ~p, stmt: ~p", [Type, Id, Q]),
+        execute(Type, Q, Socket) end, Queries).
 
 execute(query, Query, Socket) ->
     ok = gen_tcp:send(Socket, term_to_binary({load_query, {Query, [benchmark_test], [], []}}));
